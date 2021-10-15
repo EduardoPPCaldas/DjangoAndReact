cd9904a46601374c1f830ef7a8e48685091ee29f
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-10-15 01:07:40 -0300

    Add: Messages for Sucess add and delete leads

--- frontend/src/actions/messages.js
+++ frontend/src/actions/messages.js
@@ -1,7 +1,7 @@
 import { CREATE_MESSAGE } from "./types";
 
 // CREATE MESSAGE
-export const createMEssage = msg => {
+export const createMessage = msg => {
   return {
     type: CREATE_MESSAGE,
     payload: msg
