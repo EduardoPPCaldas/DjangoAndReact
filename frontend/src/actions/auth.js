3cd6eef345170d46110fa7b8d5942e34ba3ed1e5
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-10-18 10:30:22 -0300

    Add: Token config helper function

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -7,19 +7,7 @@ import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL , LOGO
 export const loadUser = () => (dispatch, getState) => {
   dispatch({ type: USER_LOADING })
 
-  const token = getState().auth.token
-
-  const config = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-
-  if(token){
-    config.headers['Authorization'] = `Token ${token}` 
-  }
-
-  axios.get("/api/auth/user", config).then(res =>{
+  axios.get("/api/auth/user", tokenConfig(getState)).then(res =>{
     dispatch({
       type: USER_LOADED,
       payload: res.data
@@ -58,6 +46,18 @@ export const login = (username, password) => (dispatch) => {
 
 // LOGOUT USER
 export const logout = () => (dispatch, getState) => {
+  axios.post("/api/auth/logout", null ,tokenConfig(getState)).then(res =>{
+    dispatch({
+      type: LOGOUT_SUCCESS,
+      payload: res.data
+    })
+  }).catch(err => {
+    dispatch(returnErrors(err.response.data, err.response.status))
+  })
+}
+
+// Setup config with token - helper function
+export const tokenConfig = getState => {
   const token = getState().auth.token
 
   const config = {
@@ -70,12 +70,5 @@ export const logout = () => (dispatch, getState) => {
     config.headers['Authorization'] = `Token ${token}` 
   }
 
-  axios.post("/api/auth/logout", null ,config).then(res =>{
-    dispatch({
-      type: LOGOUT_SUCCESS,
-      payload: res.data
-    })
-  }).catch(err => {
-    dispatch(returnErrors(err.response.data, err.response.status))
-  })
+  return config
 }
